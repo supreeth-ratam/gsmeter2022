863ec880845f01d03d4210d83765af520cac2571
supreeth <supreethratam@gmail.com>
2023-01-10 17:35:44 +0530

    Incomplete point

---
+++ src/components/Points/IP.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function IP({children}) {
+  
+    return (
+        <div className="points-flex">
+          <i class="uil uil-question blue"></i>
+          <div>{children}</div>
+        </div>
+      );
+  
+}
